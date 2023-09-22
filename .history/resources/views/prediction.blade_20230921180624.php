19fde4d5ce4109bdba61bc59fe4139ff2d9eba70
Hamdiya <hamdiyatietiembou28@gmail.com>
2023-09-22 20:50:48 +0200

    this is a new

---
+++ .history/resources/views/prediction.blade_20230921180624.php
@@ -0,0 +1,5 @@
+@extends("menu")
+@section('content')
+@end
+</section>
+
---
+++ .history/resources/views/prediction.blade_20230922204224.php
@@ -0,0 +1,36 @@
+@extends("menu")
+@section('content')
+<meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.10.0/css/bootstrap-datepicker.min.css">
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            <title>Surveillance</title>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
+
+
